099b02f88dac3a978af642e9edf82861c9308cac
alyona2204 <alerch@yandex.ru>
2025-09-29 14:58:00 +0300

    Задание 2 по функциям

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -29,19 +29,45 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
-
+    if (arr.length === 0) return 0; 
+    return arr.reduce((sum, current) => sum + current, 0); 
 }
 
 function differenceMaxMinWorker(...arr) {
-
+    if (arr.length === 0) return 0; 
+    const max = Math.max(...arr); 
+    const min = Math.min(...arr); 
+    return max - min; 
 }
 
 function differenceEvenOddWorker(...arr) {
+    if (arr.length === 0) return 0; 
+    let sumEven = 0; 
+    let sumOdd = 0; 
 
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sumEven += num; 
+        } else {
+            sumOdd += num; 
+        }
+    }
+    return sumEven - sumOdd; 
 }
 
 function averageEvenElementsWorker(...arr) {
+    if (arr.length === 0) return 0; 
+    let sumEven = 0; 
+    let countEven = 0; 
 
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            sumEven += num; 
+            countEven++; 
+        }
+    }
+
+    return countEven === 0 ? 0 : sumEven / countEven; 
 }
 
 function makeWork (arrOfArr, func) {
