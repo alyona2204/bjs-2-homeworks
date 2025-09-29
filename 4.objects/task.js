4f7c178043afa5fcd0c280efc5b08e986a1411d5
alyona2204 <alerch@yandex.ru>
2025-09-29 15:34:25 +0300

    Задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name;        // Имя студента
+    this.gender = gender;    // Пол студента
+    this.age = age;          // Возраст студента
+    this.marks = [];         // Массив оценок
+    this.subject = null;     // Предмет (по умолчанию null)
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName; 
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd); 
+    }
+};
 
-Student.prototype.getAverage = function () {
-  
-}
+Student.prototype.getAverage = function() {
+    if (this.marks.length === 0) {
+        return 0; 
+    }
+    const sum = this.marks.reduce((accum, mark) => accum + mark, 0); 
+    return sum / this.marks.length; 
+};
 
-Student.prototype.exclude = function (reason) {
-  
-}
+Student.prototype.exclude = function(reason) {
+    delete this.subject; 
+    delete this.marks;   
+    this.excluded = reason; 
+};
+
