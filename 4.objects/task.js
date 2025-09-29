374b0a6888caf63a16daeddd3e0f10793f664c62
alyona2204 <alerch@yandex.ru>
2025-09-29 16:20:01 +0300

    test

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -33,16 +33,3 @@ Student.prototype.exclude = function(reason) {
     this.excluded = true;
     this.excludeReason = reason;
 };
-
-let student1 = new Student("Василиса", "женский", 19);
-student1.setSubject("Algebra");
-console.log(student1.getAverage()); // 0
-student1.addMarks(4, 5, 4, 5);
-console.log(student1.getAverage()); // 4.5
- 
-
-let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject("Geometry");
-student2.exclude('плохая учёба');
-console.log(student2);
-
