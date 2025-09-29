406aad5ddc6e9478dbb5e93079dda4c38cee89da
alyona2204 <alerch@yandex.ru>
2025-09-29 14:36:27 +0300

    Задание 1 по функциям

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,31 @@
 function getArrayParams(...arr) {
+    let min = arr[0];
+    let max = arr[0];
+    let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+    if (arr.length === 0) {
+        return { min: 0, max: 0, avg: 0 };
+    }
+
+    for (let num of arr) {
+        if (num < min) {
+            min = num;
+        }
+
+        if (num > max) {
+            max = num;
+        }
+
+        sum += num;
+    }
+
+    const avg = (sum / arr.length).toFixed(2);
+
+    return {
+        min: min,
+        max: max,
+        avg: Number(avg) 
+    };
 }
 
 function summElementsWorker(...arr) {
