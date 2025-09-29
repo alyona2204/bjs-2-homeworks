0ed8d78dec80a80214c9c125ff1fa71d0bd75def
alyona2204 <alerch@yandex.ru>
2025-09-29 15:49:58 +0300

    for tests

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,20 +5,20 @@ function Student(name, gender, age) {
     this.marks = [];         // Массив оценок
     this.subject = null;     // Предмет (по умолчанию null)
 }
-//
+
 Student.prototype.setSubject = function(subjectName) {
     this.subject = subjectName; 
 };
 
 Student.prototype.addMarks = function(...marksToAdd) {
-    if (this.marks) {
+    if (this.marks !== undefined) {
         this.marks.push(...marksToAdd); 
     }
 };
 
 
 Student.prototype.getAverage = function() {
-    if (this.marks.length === 0) {
+    if (!this.marks || this.marks.length === 0) {
         return 0; 
     }
     const sum = this.marks.reduce((accum, mark) => accum + mark, 0); 
@@ -26,8 +26,20 @@ Student.prototype.getAverage = function() {
 };
 
 Student.prototype.exclude = function(reason) {
-    delete this.subject; 
-    delete this.marks;   
+    this.subject = undefined; 
+    this.marks = undefined;  
     this.excluded = reason; 
 };
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1); // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
+console.log(student2); // {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
 
